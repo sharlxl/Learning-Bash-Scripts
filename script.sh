e0cfc4dcef78ae6c236d8f781adb2082c5644d5c
xHappy <82674933+sharlxl@users.noreply.github.com>
2022-08-11 14:41:13 +0700

    Update script.sh

--- script.sh
+++ script.sh
@@ -3,76 +3,75 @@
 COUNTER=0
 
 #1 Ensure SSH root login is disabled
-ROOT_LOGIN=$(grep "^PermitRootLogin" /etc/ssh/sshd_config)
+grep -i "^\s*PermitRootLogin\s*no\s*$" /etc/ssh/sshd_config
 
-if [ "$ROOT_LOGIN" == "PermitRootLogin no" ]
+if [ $? -ne 0 ]
 then
-    CHECK_1="PASS"
+	CHECK_1="FAILED"
+	((COUNTER++))
 else
-    CHECK_1="FAILED"
-    ((COUNTER++))
+	CHECK_1="PASS"
 fi
 
 #2 Ensure SSH empty password disabled.
-EMPTY_PW=$(grep "^PermitEmptyPasswords" /etc/ssh/sshd_config)
+grep -i "^\s*PermitEmptyPasswords\s*no\s*$" /etc/ssh/sshd_config
 
-if [ "$EMPTY_PW" == "PermitEmptyPasswords no" ]
+if [ $? -ne 0 ]
 then
-    	CHECK_2="PASS"
+        CHECK_2="FAILED"
+        ((COUNTER++))
 else
-    	CHECK_2="FAILED"
-    ((COUNTER++))
+        CHECK_2="PASS"
 fi
 
 #3 Ensure SSH protocol set to 2.
-PROTOCOL=$(ssh -Q protocol-version localhost)
+#PROTOCOL=$(ssh -Q protocol-version localhost)
+grep -i "^\s*Protocol\s*2\s*$" /etc/ssh/sshd_config
 
-if [ $PROTOCOL == 2 ]
+if [ $? -ne 0 ]
 then
-    	CHECK_3="PASS"
+        CHECK_3="FAILED"
+        ((COUNTER++))
 else
-    	CHECK_3="FAILED"
-    ((COUNTER++))
+        CHECK_3="PASS"
 fi
 
-#4 Ensure password expiration is 90days or less.
-PW_EXPIRY=$(grep "^PASS_MAX_DAYS" /etc/login.defs | xargs | cut -d " " -f 2
-)
+#4 Ensure password expiration is 90 days or less.
+grep -iE "^\s*PASS_MAX_DAYS\s*(?:\d|[0-8][0-9]|90)\s*$" /etc/login.defs
 
-if [ $PW_EXPIRY -lt 91 ]
+if [ $? -ne 0 ]
 then
-    	CHECK_4="PASS"
+	CHECK_4="FAILED"
+	((COUNTER++))
 else
-    	CHECK_4="FAILED"
-    ((COUNTER++))
+	CHECK_4="PASS"
 fi
 
 #5 Ensure system accounts are non login.
-LOGIN=$(egrep -v "^\+" /etc/passwd | awk -F: '($1!="root" && $1!="sync" && $1!="shutdown" && $1!="halt" && $3<1000 && $7!="/usr/sbin/nologin" && $7!="/bin/false") {print}' )
+LOGIN=$(egrep -v "^\+" /etc/passwd | awk -F: '($1!="root" && $1!="sync" && $1!="shutdown" && $1!="halt" && $3<1000 && $7!="/usr/sbin/nologin" && $7!="/bin/false") {print}')
 
 if [ "$LOGIN" == "" ]
 then
     	CHECK_5="PASS"
 else
     	CHECK_5="FAILED"
-    ((COUNTER++))
+	((COUNTER++))
 fi
 
 if [ $COUNTER == 0 ]
 then
     RESULT="ALL COMPLIANCE TESTS PASSED"
 else
-    RESULT="$COUNTER/5 FAILED"
+    RESULT="$COUNTER/5 SSH COMPLIANCE CHECK FAILED"
 fi
 
-echo "===SSH Compliance Check ($(date +%d/%m/%Y" "%R))==="
-echo "1.[$CHECK_1]Ensure Root Login disabled"
-echo "2.[$CHECK_2]Ensure Permit Empty Password disabled"
-echo "3.[$CHECK_3]Ensure SSH Protocol is set to 2"
-echo "4.[$CHECK_4]Ensure Password expiration is 90days or less"
-echo "5.[$CHECK_5]Ensure system accounts are non-login"
-echo "===[$RESULT]==="
-echo " "
+echo "$(date +%d/%m/%Y" "%R)"
+echo "===[ $RESULT ]==="
+echo "1.[$CHECK_1] - Ensure SSH root login is disabled"
+echo "2.[$CHECK_2] - Ensure SSH PermitEmptyPasswords is disabled"
+echo "3.[$CHECK_3] - Ensure SSH Protocol is set to 2"
+echo "4.[$CHECK_4] - Ensure Password Expiry is 90 or less days"
+echo "5.[$CHECK_5] - Ensure system accounts are non-login"
 
-# crontab -e
-# * 8,17 * * * ~/Desktop/assignment1.sh >> ~/Desktop/assignment1.log
+#* 8,17 * * * ~/Desktop/Assignment_1/script.sh >> ~/Desktop/Assignment_1/assignment1.log
+
